b69ab29e5535d240dfb6eb798d75df05ca0e02b3
Robert B <rbrough3@charlotte.edu>
2025-03-31 23:12:04 -0400

    Feat: Added database creation & table creation querys to the database implementation

--- react-app/src/database.js
+++ react-app/src/database.js
@@ -3,10 +3,19 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Charlotte2025!"
+  password: "Hope8109",
+  database: "ITSC4155"
 });
 
 con.connect(function(err) {
   if (err) throw err;
-  console.log("Connected!");
+    console.log("Connected!");
+    con.query("CREATE DATABASE ITSC4155", function (err, result) {
+        if (err) throw err;
+            console.log("Database created");
+    });
+    con.query("CREATE TABLE login (user VARCHAR(255), password VARCHAR(255))", function (err, result) {
+        if (err) throw err;
+        console.log("Table created");
+    });
 });
